a0acefee133cff77d16d2810ed4a182c6da4c892
Saya <s_amangeldinova@kbtu.kz>
2024-04-07 02:00:52 +0500

    addded animal list fragment

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -1,49 +1,24 @@
 package com.example.lab2
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.example.lab2.adapter.AnimalAdapter
+import androidx.appcompat.app.AppCompatActivity
 import com.example.lab2.databinding.ActivityMainBinding
-import com.example.lab2.model.Animal
-import com.example.lab2.model.AnimalDataSource
+import com.example.lab2.fragment.AnimalListFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    private var adapter: AnimalAdapter? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        adapter = AnimalAdapter(
-            onAnimalClick = {
-                handleAnimalClick(it)
-            },
-            onAnimalRemoved = {
-                handleAnimalRemoval(it)
-            }
-        )
+        val animalListFragment = AnimalListFragment.newInstance()
 
-        binding.recyclerView.adapter = adapter
-        adapter?.setData(AnimalDataSource.animalList)
-    }
-
-    private fun handleAnimalClick(animal: Animal){
-        val intent = Intent(this, SecondActivity::class.java)
-        intent.putExtra(SecondActivity.KEY_RESULT, animal.name)
-        startActivity(intent)
-    }
-
-    private fun handleAnimalRemoval(animal: Animal){
-        val animalList = AnimalDataSource.animalList
-        animalList.remove(animal)
-
-        adapter?.setData(animalList)
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_container, animalListFragment)
+            .commit()
     }
 }
--- app/src/main/java/com/example/lab2/fragment/AnimalListFragment.kt
+++ app/src/main/java/com/example/lab2/fragment/AnimalListFragment.kt
@@ -1,12 +1,16 @@
 package com.example.lab2.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.lab2.R
+import com.example.lab2.SecondActivity
+import com.example.lab2.adapter.AnimalAdapter
 import com.example.lab2.databinding.FragmentAnimalListBinding
+import com.example.lab2.model.Animal
+import com.example.lab2.model.AnimalDataSource
 
 class AnimalListFragment : Fragment() {
 
@@ -16,12 +20,44 @@ class AnimalListFragment : Fragment() {
     companion object {
         fun newInstance() = AnimalListFragment()
     }
+
+    private var adapter: AnimalAdapter? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentAnimalListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = AnimalAdapter(
+            onAnimalClick = {
+                handleAnimalClick(it)
+            },
+            onAnimalRemoved = {
+                handleAnimalRemoval(it)
+            }
+        )
+
+        binding.recyclerView.adapter = adapter
+        adapter?.setData(AnimalDataSource.animalList)
+    }
+
+    private fun handleAnimalClick(animal: Animal){
+        val intent = Intent(requireContext(), SecondActivity::class.java)
+        intent.putExtra(SecondActivity.KEY_RESULT, animal.name)
+        startActivity(intent)
+    }
+
+    private fun handleAnimalRemoval(animal: Animal){
+        val animalList = AnimalDataSource.animalList
+        animalList.remove(animal)
+
+        adapter?.setData(animalList)
     }
 
 
